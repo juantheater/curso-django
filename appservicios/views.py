99a77fdfb7ed2f333050b3c23fd57d5c41799335
juan <jctpicon@gmail.com>
2026-01-13 15:50:31 -0400

    primera version

---
+++ appservicios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Servicio
+
+
+# Create your views here.
+def servicios(request):
+    
+    servicios=Servicio.objects.all()
+    return render(request,'Servicios/servicios.html',{"servicios":servicios})
